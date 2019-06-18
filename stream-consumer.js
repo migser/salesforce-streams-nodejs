dba06ca1b71dfd5fa84e23816ec1269e85b834d4
Miguel Serrano <mserranocaldeiro@mserrano-ltm3.internal.salesforce.com>
2019-06-18 17:42:10 +0200

    interpretando mensaje

--- stream-consumer.js
+++ stream-consumer.js
@@ -32,9 +32,9 @@ redisClient.on("error", (err) => {
 console.log('Esperando Mensajes!');
 // For each incoming message:
 const messageCallback = (message, salesforceApi) => {
-  console.error(`Recibido mensaje:${message}`);
+  console.error(`Recibido mensaje: ${JSON.stringify(message)}`);
   const resultado = JSON.stringify(publishKafka(message, 'CDC'));
-  console.error(`Resuñtado de kafka publish: ${resultado}`);
+  console.error(`Resultado de kafka publish: ${resultado}`);
   const redisMulti = redisClient.multi();
   const execMultiAsync = promisify(redisMulti.exec).bind(redisMulti);
   // Populate more details of the message (like User name & Account name)
